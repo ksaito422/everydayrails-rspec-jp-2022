4a7e640cd957c59919aebf0a5ab5a29e87f3e368
ksaito <kitsit.39@gmail.com>
2023-12-28 10:29:07 +0900

    11-tdd: hanson

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -19,4 +19,18 @@ RSpec.describe "Projects", type: :system do
       end
     end.to change(user.projects, :count).by(1)
   end
+
+  scenario 'user completes a project' do
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project, owner: user)
+    sign_in user
+
+    visit project_path(project)
+    click_button 'Complete'
+
+    expect(project.reload.completed?).to be true
+    expect(page).to have_content 'Congratulations, this project is complete!'
+    expect(page).to have_content 'Completed'
+    expect(page).not_to have_button 'Complete'
+  end
 end
